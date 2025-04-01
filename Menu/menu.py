34267e183e47e1d5d89681c90141bab8848ffa7f
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2025-04-01 18:37:27 +0300

    Delete Menu/menu.py

--- Menu/menu.py
+++
@@ -1,9 +0,0 @@
- import qrcode 
-
-menu_url = "https://drive.google.com/drive/folders/1ZbYQY0BYqqu4oqLiL1R6dvuf_xbZRQj_?usp=drive_link"
-
-qr = qrcode.make(menu_url)
-
-qr.save("menu_qr.png")
-
-print("QR code saved as 'menu_qr.png'")
